dff0591301d30c801719fe494c1e9f9a7501b2d1
Andrey Mokretsov <am@kubia.com>
2020-09-13 15:19:57 +0500

    cors

--- src/modules/v1/BaseController.php
+++ src/modules/v1/BaseController.php
@@ -20,6 +20,23 @@ abstract class BaseController extends ActiveController
     {
         $behaviors = parent::behaviors();
         $behaviors['contentNegotiator']['formats']['text/html'] = Response::FORMAT_JSON;
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::className(),
+            'cors' => [
+                // restrict access to
+                'Origin' => ['*'],
+                // Allow only POST and PUT methods
+                'Access-Control-Request-Method' => ['GET', 'HEAD', 'OPTIONS'],
+                // Allow only headers 'X-Wsse'
+                //'Access-Control-Request-Headers' => ['X-Wsse'],
+                // Allow credentials (cookies, authorization headers, etc.) to be exposed to the browser
+                'Access-Control-Allow-Credentials' => true,
+                // Allow OPTIONS caching
+                'Access-Control-Max-Age' => 3600,
+                // Allow the X-Pagination-Current-Page header to be exposed to the browser.
+                'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page'],
+            ],
+        ];
 
         if(YII_ENV !== 'dev') {
             $behaviors['httpCacheIndex'] = [
